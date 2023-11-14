001b725c5321a20bd383c6e83b96ccfbb999e852
Rifti <alwirifti@gmail.com>
2023-11-14 13:15:58 +0700

    custom component

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,18 +15,31 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.all(9),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(color: Colors.red),
-            child: Text("Persegi"),
-          ),
-          Container(
-            margin: EdgeInsets.all(9),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(color: Colors.red),
-            child: Text("SegiTiga"),
-          ),
+          custommenu(imageAssets: "assets/Untitled.png",title: "Persegi"),
+          custommenu(imageAssets: "assets/segitiga.png",title: "segitiga"),
+        ],
+      ),
+    );
+  }
+}
+
+class custommenu extends StatelessWidget {
+  const custommenu({
+    super.key, required this.imageAssets, required this.title,
+  });
+  final String imageAssets;sS
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(9),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(color: Colors.deepOrange[400]),
+      child: Column(
+        children: [
+          Image.asset(imageAssets,height: 100,),
+          Text(title),
         ],
       ),
     );
